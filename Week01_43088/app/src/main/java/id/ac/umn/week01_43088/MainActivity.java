ea244ea9c36d331281ec7b422245f4923a18d62c
Fernando Khorasani <fernando.khorasani@gmail.com>
2022-08-31 09:10:39 +0700

    First Week
    Changing projects name and "Hello World!" string

---
+++ Week01_43088/app/src/main/java/id/ac/umn/week01_43088/MainActivity.java
@@ -0,0 +1,19 @@
+package id.ac.umn.week01_43088;
+
+//import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+//import android.support.v7.widget.RecyclerView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
